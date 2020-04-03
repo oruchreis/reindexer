c9a7c68a7683622e4d1b0497b4c8f717ba442205
oruchreis <oruchreis@diyezon.com>
2020-04-03 16:17:50 +0300

    Exported cbinding functions for shared library build.

--- bindings/builtinserver/builtinserver_posix.go
+++ bindings/builtinserver/builtinserver_posix.go
@@ -1,9 +1,9 @@
 // +build !windows
-//go:generate sh -c "cd ../.. && mkdir -p build && cd build && cmake -DGO_BUILTIN_SERVER_EXPORT_PKG_PATH=\"../../bindings/builtinserver\" .. && make reindexer_server_library reindexer -j8"
+//go:generate sh -c "cd ../.. && mkdir -p build && cd build && cmake -DGO_BUILTIN_SERVER_EXPORT_PKG_PATH=\"../../bindings/builtinserver\" .. && make reindexer_server_library reindexer -j12"
 
 package builtinserver
 
 // #cgo CXXFLAGS: -std=c++11 -g -O2 -Wall -Wpedantic -Wextra -I../../cpp_src 
 // #cgo CFLAGS: -std=c99 -g -O2 -Wall -Wpedantic -Wno-unused-variable -I../../cpp_src 
-// #cgo LDFLAGS: -L${SRCDIR}/../../build/cpp_src/ -L${SRCDIR}/../../build/cpp_src/server/ -lreindexer_server_library  -lreindexer -lleveldb -lsnappy -ldl -lreindexer_server_resources -lm -lstdc++ -g
+// #cgo LDFLAGS: -L${SRCDIR}/../../build/cpp_src/ -L${SRCDIR}/../../build/cpp_src/server/ -lreindexer_server_library  -lreindexer -lleveldb -lsnappy -lpthread -lrt -ldl -lunwind -lreindexer_server_resources -lm -lstdc++ -g
 import "C"
--- cpp_src/core/cbinding/reindexer_c.h
+++ cpp_src/core/cbinding/reindexer_c.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+#else
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -7,57 +17,57 @@ extern "C" {
 #include <stdint.h>
 #include "reindexer_ctypes.h"
 
-uintptr_t init_reindexer();
-void destroy_reindexer(uintptr_t rx);
+EXPORT uintptr_t init_reindexer();
+EXPORT void destroy_reindexer(uintptr_t rx);
 
-reindexer_error reindexer_connect(uintptr_t rx, reindexer_string dsn, ConnectOpts opts, reindexer_string client_vers);
-reindexer_error reindexer_ping(uintptr_t rx);
+EXPORT reindexer_error reindexer_connect(uintptr_t rx, reindexer_string dsn, ConnectOpts opts, reindexer_string client_vers);
+EXPORT reindexer_error reindexer_ping(uintptr_t rx);
 
-reindexer_error reindexer_enable_storage(uintptr_t rx, reindexer_string path, reindexer_ctx_info ctx_info);
-reindexer_error reindexer_init_system_namespaces(uintptr_t rx);
+EXPORT reindexer_error reindexer_enable_storage(uintptr_t rx, reindexer_string path, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_init_system_namespaces(uintptr_t rx);
 
-reindexer_error reindexer_open_namespace(uintptr_t rx, reindexer_string nsName, StorageOpts opts, reindexer_ctx_info ctx_info);
-reindexer_error reindexer_drop_namespace(uintptr_t rx, reindexer_string nsName, reindexer_ctx_info ctx_info);
-reindexer_error reindexer_truncate_namespace(uintptr_t rx, reindexer_string nsName, reindexer_ctx_info ctx_info);
-reindexer_error reindexer_rename_namespace(uintptr_t rx, reindexer_string srcNsName, reindexer_string dstNsName,
+EXPORT reindexer_error reindexer_open_namespace(uintptr_t rx, reindexer_string nsName, StorageOpts opts, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_drop_namespace(uintptr_t rx, reindexer_string nsName, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_truncate_namespace(uintptr_t rx, reindexer_string nsName, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_rename_namespace(uintptr_t rx, reindexer_string srcNsName, reindexer_string dstNsName,
 										   reindexer_ctx_info ctx_info);
-reindexer_error reindexer_close_namespace(uintptr_t rx, reindexer_string nsName, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_close_namespace(uintptr_t rx, reindexer_string nsName, reindexer_ctx_info ctx_info);
 
-reindexer_error reindexer_add_index(uintptr_t rx, reindexer_string nsName, reindexer_string indexDefJson, reindexer_ctx_info ctx_info);
-reindexer_error reindexer_update_index(uintptr_t rx, reindexer_string nsName, reindexer_string indexDefJson, reindexer_ctx_info ctx_info);
-reindexer_error reindexer_drop_index(uintptr_t rx, reindexer_string nsName, reindexer_string index, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_add_index(uintptr_t rx, reindexer_string nsName, reindexer_string indexDefJson, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_update_index(uintptr_t rx, reindexer_string nsName, reindexer_string indexDefJson, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_drop_index(uintptr_t rx, reindexer_string nsName, reindexer_string index, reindexer_ctx_info ctx_info);
 
-reindexer_tx_ret reindexer_start_transaction(uintptr_t rx, reindexer_string nsName);
+EXPORT reindexer_tx_ret reindexer_start_transaction(uintptr_t rx, reindexer_string nsName);
 
-reindexer_error reindexer_modify_item_packed_tx(uintptr_t rx, uintptr_t tr, reindexer_buffer args, reindexer_buffer data);
-reindexer_error reindexer_update_query_tx(uintptr_t rx, uintptr_t tr, reindexer_buffer in);
-reindexer_error reindexer_delete_query_tx(uintptr_t rx, uintptr_t tr, reindexer_buffer in);
-reindexer_ret reindexer_commit_transaction(uintptr_t rx, uintptr_t tr, reindexer_ctx_info ctx_info);
+EXPORT reindexer_error reindexer_modify_item_packed_tx(uintptr_t rx, uintptr_t tr, reindexer_buffer args, reindexer_buffer data);
+EXPORT reindexer_error reindexer_update_query_tx(uintptr_t rx, uintptr_t tr, reindexer_buffer in);
+EXPORT reindexer_error reindexer_delete_query_tx(uintptr_t rx, uintptr_t tr, reindexer_buffer in);
+EXPORT reindexer_ret reindexer_commit_transaction(uintptr_t rx, uintptr_t tr, reindexer_ctx_info ctx_info);
 
-reindexer_error reindexer_rollback_transaction(uintptr_t rx, uintptr_t tr);
+EXPORT reindexer_error reindexer_rollback_transaction(uintptr_t rx, uintptr_t tr);
 
-reindexer_ret reindexer_modify_item_packed(uintptr_t rx, reindexer_buffer args, reindexer_buffer data, reindexer_ctx_info ctx_info);
-reindexer_ret reindexer_select(uintptr_t rx, reindexer_string query, int as_json, int32_t *pt_versions, int pt_versions_count,
+EXPORT reindexer_ret reindexer_modify_item_packed(uintptr_t rx, reindexer_buffer args, reindexer_buffer data, reindexer_ctx_info ctx_info);
+EXPORT reindexer_ret reindexer_select(uintptr_t rx, reindexer_string query, int as_json, int32_t *pt_versions, int pt_versions_count,
 							   reindexer_ctx_info ctx_info);
 
-reindexer_ret reindexer_select_query(uintptr_t rx, reindexer_buffer in, int as_json, int32_t *pt_versions, int pt_versions_count,
+EXPORT reindexer_ret reindexer_select_query(uintptr_t rx, reindexer_buffer in, int as_json, int32_t *pt_versions, int pt_versions_count,
 									 reindexer_ctx_info ctx_info);
-reindexer_ret reindexer_delete_query(uintptr_t rx, reindexer_buffer in, reindexer_ctx_info ctx_info);
-reindexer_ret reindexer_update_query(uintptr_t rx, reindexer_buffer in, reindexer_ctx_info ctx_info);
+EXPORT reindexer_ret reindexer_delete_query(uintptr_t rx, reindexer_buffer in, reindexer_ctx_info ctx_info);
+EXPORT reindexer_ret reindexer_update_query(uintptr_t rx, reindexer_buffer in, reindexer_ctx_info ctx_info);
 
-reindexer_error reindexer_free_buffer(reindexer_resbuffer in);
-reindexer_error reindexer_free_buffers(reindexer_resbuffer *in, int count);
+EXPORT reindexer_error reindexer_free_buffer(reindexer_resbuffer in);
+EXPORT reindexer_error reindexer_free_buffers(reindexer_resbuffer *in, int count);
 
-reindexer_error reindexer_commit(uintptr_t rx, reindexer_string nsName);
+EXPORT reindexer_error reindexer_commit(uintptr_t rx, reindexer_string nsName);
 
-reindexer_error reindexer_put_meta(uintptr_t rx, reindexer_string ns, reindexer_string key, reindexer_string data,
+EXPORT reindexer_error reindexer_put_meta(uintptr_t rx, reindexer_string ns, reindexer_string key, reindexer_string data,
 								   reindexer_ctx_info ctx_info);
-reindexer_ret reindexer_get_meta(uintptr_t rx, reindexer_string ns, reindexer_string key, reindexer_ctx_info ctx_info);
+EXPORT reindexer_ret reindexer_get_meta(uintptr_t rx, reindexer_string ns, reindexer_string key, reindexer_ctx_info ctx_info);
 
-reindexer_error reindexer_cancel_context(reindexer_ctx_info ctx_info, ctx_cancel_type how);
+EXPORT reindexer_error reindexer_cancel_context(reindexer_ctx_info ctx_info, ctx_cancel_type how);
 
-void reindexer_enable_logger(void (*logWriter)(int level, char *msg));
-void reindexer_disable_logger();
+EXPORT void reindexer_enable_logger(void (*logWriter)(int level, char *msg));
+EXPORT void reindexer_disable_logger();
 
 #ifdef __cplusplus
 }
