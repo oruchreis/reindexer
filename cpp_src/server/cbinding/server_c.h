fee7b5bbf52b8f11208e89c9b69f5edd34d88167
oruchreis <oruchreis@diyezon.com>
2020-04-03 16:17:50 +0300

    Exported core/server cbinding functions for shared library build.

--- cpp_src/server/cbinding/server_c.h
+++ cpp_src/server/cbinding/server_c.h
@@ -1,20 +1,30 @@
 
 #pragma once
 
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+#else
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include "core/cbinding/reindexer_ctypes.h"
 
-uintptr_t init_reindexer_server(void);
-void destroy_reindexer_server(uintptr_t psvc);
-reindexer_error start_reindexer_server(uintptr_t psvc, reindexer_string config);
-reindexer_error stop_reindexer_server(uintptr_t psvc);
-reindexer_error get_reindexer_instance(uintptr_t psvc, reindexer_string dbname, reindexer_string user, reindexer_string pass,
+EXPORT uintptr_t init_reindexer_server(void);
+EXPORT void destroy_reindexer_server(uintptr_t psvc);
+EXPORT reindexer_error start_reindexer_server(uintptr_t psvc, reindexer_string config);
+EXPORT reindexer_error stop_reindexer_server(uintptr_t psvc);
+EXPORT reindexer_error get_reindexer_instance(uintptr_t psvc, reindexer_string dbname, reindexer_string user, reindexer_string pass,
 									   uintptr_t* rx);
-int check_server_ready(uintptr_t psvc);
-reindexer_error reopen_log_files(uintptr_t psvc);
+EXPORT int check_server_ready(uintptr_t psvc);
+EXPORT reindexer_error reopen_log_files(uintptr_t psvc);
 
 #ifdef __cplusplus
 }
