cfaa77bfd09301e1a99f5fa927a759765168c328
oruchreis <oruchreis@diyezon.com>
2023-01-06 15:47:11 +0300

    Renamed malloc_free

--- cpp_src/server/grpc/grpcexport.h
+++ cpp_src/server/grpc/grpcexport.h
@@ -9,11 +9,26 @@ class DBManager;
 
 }
 
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+#else
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
+#ifdef __cplusplus
 extern "C" {
-void start_reindexer_grpc(reindexer_server::DBManager& dbMgr, std::chrono::seconds txIdleTimeout, reindexer::net::ev::dynamic_loop& loop,
+#endif
+EXPORT void* start_reindexer_grpc(reindexer_server::DBManager& dbMgr, std::chrono::seconds txIdleTimeout, reindexer::net::ev::dynamic_loop& loop,
 						  const std::string& address);
-void stop_reindexer_grpc();
+EXPORT void stop_reindexer_grpc(void*);
+#ifdef __cplusplus
 }
+#endif
+
 typedef void* (*p_start_reindexer_grpc)(reindexer_server::DBManager& dbMgr, std::chrono::seconds txIdleTimeout,
 										reindexer::net::ev::dynamic_loop& loop, const std::string& address);
 
